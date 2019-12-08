88df21608f870edda69ebcf283581f9fcac1b3ce
Prakritishrivastava <48962118+Prakritishrivastava@users.noreply.github.com>
2019-12-08 23:54:56 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,24 @@
+import re
+print("our calculator")
+print("type 'quite' to exit \n ")
+previous=0
+run=True
+def performmath():
+    global run
+    global previous
+    equ=""
+    if previous==0:
+     equ=input("enter the equation:")
+    else:
+        equ=input(str(previous))
+    if equ=="quit":
+        run=False
+    else:
+        equ=re.sub('[a-zA-Z\,.:;""]','',equ)
+        if previous==0:
+           previous=eval(equ)
+        else:
+            previous=eval(str(previous)+equ)
+        print("you entered the equation",previous)
+while run:
+    performmath()
